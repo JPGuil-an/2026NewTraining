5988898ef6e89156361eb77ac75acae66f4fe579
JP Guil-an <jpguilan.official@gmail.com>
2026-08-27 15:58:17 +0800

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,60 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+}
+
+const DEFAULT_USERS: User[] = [
+  { id: "1", name: "John Doe 1", email: "john.doe1@example.com" },
+  { id: "2", name: "John Doe 2", email: "john.doe2@example.com" },
+  { id: "3", name: "John Doe 3", email: "john.doe3@example.com" },
+];
+
 export default function App() {
+
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setUsers(DEFAULT_USERS);
+    }, 3000);
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
     <div>
       <h1>Users Lists</h1>
-      <div>
-        <div>
-          <h2>John Doe 1</h2>
-          <p>john.doe1@example.com</p>
-          <p>12345678901</p>
-          <p>12345678902</p>
-        </div>
-        <div>
-          <h2>John Doe 2</h2>
-          <p>john.doe2@example.com</p>
-          <p>12345678903</p>
-          <p>12345678904</p>
-        </div>
-        <div>
-          <h2>John Doe 3</h2>
-          <p>john.doe3@example.com</p>
-          <p>12345678905</p>
-          <p>12345678906</p>
-        </div>
+      <div className="flex flex-col gap-4">
+        {users.map((user) => (
+          <UserCard
+            key={user.id}
+            id={user.id}
+            name={user.name}
+            email={user.email}
+          />
+        ))}
       </div>
+    </div>
 
+  )
+}
+
+type UserCardProps = User;
+//function here is called a component and it is used to render the user card
+// inside the function we are passing the name and email as props
+// props are the data that is passed to the component
+function UserCard(props: UserCardProps) {
+  return (
+    <div>
+      <div className="bg-gray-100">
+      <p>{props.id}</p>
+      <h2>{props.name}</h2>
+      <p>{props.email}</p>
+      </div>
     </div>
   )
 }
+
